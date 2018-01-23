5a9e7327a20092df0644397fc3a512ba5f274ec4
lapin <lapin@kontur.ru>
2018-01-23 10:37:18 +0400

    first commit

---
+++ Vostok.ClusterClient.Transport.Http/AssemblyProperties.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Vostok.ClusterClient.Transport.Http.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ Vostok.ClusterClient.Transport.Http/ClusterClientConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Vostok.Clusterclient.Transport.Http
+{
+    public static class ClusterClientConfigurationExtensions
+    {
+        /// <summary>
+        /// Initialiazes configuration transport with a <see cref="VostokHttpTransport"/>.
+        /// </summary>
+        public static void SetupVostokHttpTransport(this IClusterClientConfiguration configuration)
+        {
+            configuration.Transport = new VostokHttpTransport(configuration.Log);
+        }
+    }
+}
